692331781e1f0a986099ac3ec015b3792d5b7a6e
gupta-vivek <vivek.gupta@razorthink.net>
2016-11-21 11:49:27 +0530

    Do correction.
    Modify the program to accept command line arguments.

--- script_2.sh
+++ script_2.sh
@@ -2,16 +2,15 @@
 # This is a Shell Script to accept a filename and path(dir path) as command line argument and find whether the file is present in the
 # given path. If it is present , it will delete the file otherwise it will archive the given directory.
 
-echo -n "Enter the name of the file: "
-read filename
-echo -n "Enter the directory: "
-read directory
+#!/bin/bash
+# This is a Shell Script to accept a filename and path(dir path) as command line argument and find whether the file is present in the
+# given path. If it is present , it will delete the file otherwise it will archive the given directory.
 
-if [ -f $directory/$filename ];
+if [ -f $2/$1 ];
 then
-   rm $directory/$filename
+   rm $2/$1
    printf "\nFile exists and has been removed."
 else
-   printf "\nFile $FILE does not exist.\nMade an archive of the directory\n"
-   tar -cPf $filename.tar $directory
+   printf "\nFile $1 does not exist.\nMade an archive of the directory\n"
+   tar -cPf $1.tar $2
 fi
