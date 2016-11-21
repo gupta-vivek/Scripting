70132ec3eecd23a353310a20450bd038a4c062ef
gupta-vivek <vivek.gupta@razorthink.net>
2016-11-21 11:50:02 +0530

    Remove repeated comment.

--- script_2.sh
+++ script_2.sh
@@ -2,10 +2,6 @@
 # This is a Shell Script to accept a filename and path(dir path) as command line argument and find whether the file is present in the
 # given path. If it is present , it will delete the file otherwise it will archive the given directory.
 
-#!/bin/bash
-# This is a Shell Script to accept a filename and path(dir path) as command line argument and find whether the file is present in the
-# given path. If it is present , it will delete the file otherwise it will archive the given directory.
-
 if [ -f $2/$1 ];
 then
    rm $2/$1
