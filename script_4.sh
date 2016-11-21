7c6743baaa264ed2438ffdb9f5a115f44de67d18
gupta-vivek <vivek.gupta@razorthink.net>
2016-11-21 11:47:46 +0530

    Do correction.

--- script_4.sh
+++ script_4.sh
@@ -2,22 +2,15 @@
 # This is a shell script to accept two filenames as arguments. First file will be having the username and the second file will be having the passwords for it.
 # If the number of usernames doesn't match with number of passwords it will not create any users, if it matches it will create user with corresponding password.
 
-printf "\nEnter the name of the file(Username): "
-read usernameFile
-printf "\nEnter the name of the file(Password): "
-read passwordFile
-
-userLineCount=$(cat $usernameFile | wc -l )
-passwordLineCount=$(cat $passwordFile | wc -l )
+userLineCount=$(cat $1 | wc -l )
+passwordLineCount=$(cat $2 | wc -l )
 
 if [ "$userLineCount" -eq "$passwordLineCount" ];
 then
-	printf "\nEnter Username: "
-	read username
-	printf "\nEnter Password: "
-	read password
-	printf "\n$username" >> $usernameFile
-	printf "\n$password" >> $passwordFile
+	while read -r -u 4 username && read -r -u 5 password; do
+  		sudo useradd $username
+		sudo echo -e "$password\n$password" | passwd $username
+	done 4<$1 5<$2
 else
 	printf "The count of users and passwords do not match!"
 fi
